d5818c7950db84d1445dcbdaeec5d285aa1f47e2
HaeRi Seo <62414262+SeoHaeRi@users.noreply.github.com>
2022-01-03 22:38:41 +0900

    Add files via upload

---
+++ 009_routing_and_middleware/app1.js
@@ -0,0 +1,27 @@
+// npm init --yes
+// npm i express
+// npm i nodemon --save-dev
+// -> nodemon app1 으로 실행 가능
+const express = require("express");
+const indexRouter = require("./router"); // 뒤에 index.js 생략 가능
+const aRouter = require("./router/a.js");
+const bRouter = require("./router/b.js");
+const cRouter = require("./router/c.js");
+
+const app = express();
+app.use("/", indexRouter);
+app.use("/a", aRouter);
+app.use("/b", bRouter);
+app.use("/c", cRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send("못찾음!"); // res.send는 res.status(200).send('') // 보안상 status는 좀 더 고민해본 후 작성, 200을 일부러 보내주는 경우가 많습니다.
+});
+
+app.use((err, req, res, next) => {
+    console.log("애러발생!");
+    console.log(err);
+});
+// 매개변수가 4개인것만 애러처리합니다. 주의해주세요.
+
+app.listen(8080); // 보통 위에 있고 맨 마지막에는 error 처리하는 경우가 많음
---
+++ 009_routing_and_middleware/router/blog.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    console.log("blog.js 파일에서 /blog페이지를 처리하고 있습니다.");
+    res.send("<h1>blogRouter Contact</h1>");
+});
+
+module.exports = router;
